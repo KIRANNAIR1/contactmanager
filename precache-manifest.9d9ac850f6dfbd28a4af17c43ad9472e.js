f87d85f1a7a7f9e0b1d9565515b826e61733ae0c
KIRANNAIR1 <kirannair17@gmail.com>
2019-03-19 13:08:58 +0530

    Updates

---
+++ precache-manifest.9d9ac850f6dfbd28a4af17c43ad9472e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d5df7539dbf63550c482",
+    "url": "/contactmanager/static/css/main.11d832ee.chunk.css"
+  },
+  {
+    "revision": "d5df7539dbf63550c482",
+    "url": "/contactmanager/static/js/main.9ac35e41.chunk.js"
+  },
+  {
+    "revision": "a9b80a0b0e722e0d3664",
+    "url": "/contactmanager/static/js/runtime~main.884ecd02.js"
+  },
+  {
+    "revision": "62ff511edfb0669b2ddf",
+    "url": "/contactmanager/static/css/2.6b9b6d25.chunk.css"
+  },
+  {
+    "revision": "62ff511edfb0669b2ddf",
+    "url": "/contactmanager/static/js/2.77a9f17a.chunk.js"
+  },
+  {
+    "revision": "422928454f3b377274d306836b04dab6",
+    "url": "/contactmanager/index.html"
+  }
+];
